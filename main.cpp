d964e3c97f0d356922b3964482f51bc2e606e043
49115566 <153975225+49115566@users.noreply.github.com>
2024-04-17 13:18:35 -0500

    Update main.cpp
    Added code to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include "ItemToPurchase.h"
 using namespace std;
 
-//making a return statement
 int main(){
+	string name;
+	double price;
+	int quantity;
+	ItemToPurchase i1, i2;
+	cout << "Item 1\nEnter the item name:" << endl;
+	getline(cin, name);
+	cin.ignore();
+	i1.SetName(name);
+	cout << "Enter the item price:" << endl;
+	cin >> price;
+	i1.SetPrice(price);
+	cout << "Enter the item quantity:" << endl;
+	cin >> quantity;
+	i1.SetQuantity(quantity);
 
-  return 0;
+	cout << "\nItem 2\nEnter the item name:" << endl;
+	getline(cin, name);
+	cin.ignore();
+	i2.SetName(name);
+	cout << "Enter the item price:" << endl;
+	cin >> price;
+	i2.SetPrice(price);
+	cout << "Enter the item quantity:" << endl;
+	cin >> quantity;
+	i2.SetQuantity(quantity);
+
+	cout << "\nTOTAL COST" << endl;
+	cout << i1.GetName() << ' ' << i1.GetQuantity() << " @ $" << i1.GetQuantity() << " = $" << i1.GetPrice() * i1.GetQuantity() << endl;
+	cout << i2.GetName() << ' ' << i2.GetQuantity() << " @ $" << i2.GetQuantity() << " = $" << i2.GetPrice() * i2.GetQuantity() << endl;
+	cout << "\nTotal: $" << i1.GetPrice() * i1.GetQuantity() + i2.GetPrice() * i2.GetQuantity() << endl;
+
+	return 0;
 }
