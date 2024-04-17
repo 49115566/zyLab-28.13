c572a948c4d1fd3d21fed1201622a4a37e8b12cf
49115566 <153975225+49115566@users.noreply.github.com>
2024-04-17 13:19:39 -0500

    Update ItemToPurchase.h
    Added code to ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -4,6 +4,23 @@
 #include <string>
 using namespace std;
 
+
+class ItemToPurchase {
+public:
+	ItemToPurchase();
+	void SetName(string n);
+	void SetPrice(double p);
+	void SetQuantity(int q);
+	string GetName();
+	double GetPrice();
+	int GetQuantity();
+private:
+	string name;
+	double price;
+	int quantity;
+};
+
+
 /* code to be added here */
 
 #endif
